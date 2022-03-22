80f9e5986ca5681023763bbcf561e1b7d0a7cf29
Nopka <hugo3078@gmail.com>
2022-03-22 11:51:36 +0100

    modification bdd et ajout fonction delete User

--- reu_authentification_service/public/index.php
+++ reu_authentification_service/public/index.php
@@ -4,6 +4,7 @@ require_once  __DIR__ . '/../src/vendor/autoload.php';
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 use reu\authentification\app\bootstrap\Eloquent;
+use reu\authentification\app\controller\REUAuthController;
 
 $settings = require_once __DIR__ . '/../src/app/settings/settings.php';
 $errors = require_once __DIR__ . '/../src/app/errors/errors.php';
@@ -23,5 +24,7 @@ $app->post('/auth[/]',
 $app->post('/createUser[/]',
     reu\authentification\app\controller\REUAuthController::class.':create'
 );
+
+$app->delete('/deleteUser/{id}[/]',REUAuthController::class.':deleteUser');
 
 $app->run();
