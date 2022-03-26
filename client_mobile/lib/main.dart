5452e6525ead8654d30e7c0a70b3904456aee8de
Nopka <hugo3078@gmail.com>
2022-03-26 14:29:49 +0100

    création de la page de login pour le mobile

--- client_mobile/lib/main.dart
+++ client_mobile/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:client_mobile/components/events/event_details.dart';
 import 'package:client_mobile/data/events_collection.dart';
 import 'package:client_mobile/screens/create_event.dart';
 import 'package:client_mobile/screens/all_events.dart';
+import 'package:client_mobile/components/log_in.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -32,6 +33,7 @@ class Reunionou extends StatelessWidget {
               const CreateEvent(title: 'Créer Events'),
           EventDetails.route: (context) =>
               const EventDetails(title: 'Event Details'),
+          LoginPage.route: (context) => LoginPage(),
         });
   }
 }
